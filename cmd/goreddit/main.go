92a0abab3c0491a81fe61bfb529e6a42647391fb
Sean <seanchu@protonmail.com>
2020-08-09 20:27:03 -0700

    Login service

---
+++ cmd/goreddit/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gowebexamples/goreddit/postgres"
+	"github.com/gowebexamples/goreddit/web"
+)
+
+func main() {
+	dsn := "postgres://postgres:secret@localhost/postgres?sslmode=disable"
+
+	store, err := postgres.NewStore(dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sessions, err := web.NewSessionManager(dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csrfKey := []byte("01234567890123456789012345678901")
+	h := web.NewHandler(store, sessions, csrfKey)
+	http.ListenAndServe(":4000", h)
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/iammasariya/goreddit
+
+go 1.14
+
+require (
+	github.com/alexedwards/scs/postgresstore v0.0.0-20200729112010-8c9ddd400378 // indirect
+	github.com/alexedwards/scs/v2 v2.4.0 // indirect
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/google/uuid v1.1.1
+	github.com/gorilla/csrf v1.7.0 // indirect
+	github.com/gowebexamples/goreddit v0.0.0-20200721171152-e0a9e6336b82
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.8.0
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de // indirect
+)
